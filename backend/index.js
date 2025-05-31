241d5307be677efe968b3ca2251949ec19094c8f
Sajal1coder <157198364+Sajal1coder@users.noreply.github.com>
2025-05-31 11:31:07 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,9 @@
     dotenv.config();
 
     const app = express();
-    app.use(cors());
+    app.use(cors({
+      origin: '*'
+    }));
     app.use(express.json());
 
     const PORT = process.env.PORT || 5000; // PORT might be ignored by Vercel
